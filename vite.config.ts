50c93f75dd2c83826117e826bddfe8745b2809dd
Abhayraj B <abhayraj27052005@gmail.com>
2025-07-28 19:41:32 +0530

    the required changes has been done

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Landing-page/', 
   plugins: [react()],
-  base: '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
